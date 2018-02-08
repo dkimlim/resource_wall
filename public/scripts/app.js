b01a2bdabf0fb3823499fa878f3a6641d7352c5d
Jerry Kurian <jkurian770@gmail.com>
2018-02-08 18:53:12 -0500

    app.js: Added an onclick handler for the logout button which is a POST to /logout (clears cookie). We then redirect the window back to the home URL /
    server.js: Added a /logout POST route which clears the cookie

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,34 +6,43 @@ $(() => {
   });
 
 
-  $("#create-new-board").on('click', function (allBoards) {
+  $("#create-new-board").on('click', function (allBoards) {});
+  // $.post("/car)ds", (newCard) => {
+  //   let templateVars = { };
+  // })
+
+  $("#register-button").click(function () {
+    $(".register-form").slideToggle("slow");
   });
-    // $.post("/car)ds", (newCard) => {
-    //   let templateVars = { };
-    // })
 
-$("#register-button").click(function(){
-       $(".register-form").slideToggle("slow");
-    });
+  $("#login-button").click(function () {
+    $(".login-form").slideToggle("slow");
+  });
 
-$("#login-button").click(function(){
-        $(".login-form").slideToggle("slow");
-    });
+  $("#reveal-new-card").click(function () {
+    $("#create-card-section").slideToggle("slow");
+  });
 
-  $("#reveal-new-card").click(function(){
-     $("#create-card-section").slideToggle("slow");
-    });
-
-  $("#create-new-board").click(function(){
+  $("#create-new-board").click(function () {
     $("#new-board").slideToggle("slow");
   });
 
 
-$("#comment-box-open").click(function(){
+  $("#comment-box-open").click(function () {
     $("#comment-box").slideToggle("slow");
-     });
+  });
 
-$("#comments-display").click(function(){
+  $("#comments-display").click(function () {
     $(".comment-container").slideToggle("slow");
-     });
+  });
+
+  $("#logout-button").on('click', function () {
+    console.log("logout clicked")
+    $.post("/logout", () => {
+      console.log("logged out");
+      //This redirects the window back to the home page
+      window.location.replace("/");
+    })
+  })
+
 });
