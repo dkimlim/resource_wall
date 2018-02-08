7163f02c25098d84908c7dceeb0973b223ee016b
austin metka <austin_metka@hotmail.com>
2018-02-08 01:04:12 +0000

    changed CreateCard to it's own partial znd removed redundant Document Ready

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,16 +3,15 @@ $(() => {
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
-  });;
-});
+  });
 
-$( document ).ready(function() {
 
-$("#reveal-new-card").click(function(){
+
+  $("#reveal-new-card").click(function(){
      $("#create-card").slideToggle("slow");
     });
 
-$("#create-new-board").click(function(){
+  $("#create-new-board").click(function(){
     $("#new-board").slideToggle("slow");
      });
 });
