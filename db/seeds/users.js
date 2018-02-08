115a6b0e193c229c597d70e38e2b5874498da723
Jerry Kurian <jkurian770@gmail.com>
2018-02-07 19:32:34 -0500

    Updated Users table to contain a userid, username, email and password columns

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users').insert({userid: 1, username: 'Alice', email: 'alice@alice.com', password: 'alice'}),
+        knex('users').insert({userid: 2, username: 'Bob', email: 'bob@bob.com', password: 'bob'}),
+        knex('users').insert({userid: 3, username: 'Charlie', email: 'charlie@charlie.com', password: 'charlie'})
       ]);
     });
 };
