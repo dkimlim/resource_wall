317137708738b79b1cf4b3760b67c5c032a80746
Jerry Kurian <jkurian770@gmail.com>
2018-02-08 19:42:42 -0500

    Updated seed to not insert the primary key (it is auto inserted)

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -4,9 +4,9 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({userID: 1, username: 'Alice', email: 'alice@alice.com', password: bcrypt.hashSync('alice', 10)}),
-        knex('users').insert({userID: 2, username: 'Bob', email: 'bob@bob.com', password: bcrypt.hashSync('bob', 10)}),
-        knex('users').insert({userID: 3, username: 'Charlie', email: 'charlie@charlie.com', password: bcrypt.hashSync('charlie', 10)})
+        knex('users').insert({username: 'Alice', email: 'alice@alice.com', password: bcrypt.hashSync('alice', 10)}),
+        knex('users').insert({username: 'Bob', email: 'bob@bob.com', password: bcrypt.hashSync('bob', 10)}),
+        knex('users').insert({username: 'Charlie', email: 'charlie@charlie.com', password: bcrypt.hashSync('charlie', 10)})
       ]);
     });
 };
